f9e1010dff53ca716571a13227e7e8f96d99fc2b
aydar-gaysin <aydar-gaysin@users.noreply.github.com>
2021-01-18 22:21:56 +0500

    Corrected global variable usage

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -23,9 +23,8 @@ def fetch_spacex_last_launch(directory, spacex_api_url):
 def main():
     directory = DIR_PATH
     os.makedirs(directory, exist_ok=True)
-    spacex_api_url = SPACEX_API_URL
     try:
-        fetch_spacex_last_launch(directory, spacex_api_url)
+        fetch_spacex_last_launch(directory, SPACEX_API_URL)
     except requests.exceptions.HTTPError as error:
         exit('Ошибка:\n{0}'.format(error))
 

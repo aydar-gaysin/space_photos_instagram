a531d32c9ed44a66609e762ea269c64bcf0d48d9
aydar-gaysin <aydar-gaysin@users.noreply.github.com>
2021-01-18 22:19:27 +0500

    Added global variables: image width and height

--- upload_photos.py
+++ upload_photos.py
@@ -9,21 +9,23 @@ from PIL import Image
 
 
 DIR_PATH = 'images'
+IMAGE_WIDTH = 1080
+IMAGE_HEIGHT = 1080
 
 
-def resize_image(ig_username, ig_password, DIR_PATH):
-    for filename in listdir(DIR_PATH):
-        image = Image.open(os.path.join(DIR_PATH, filename))
-        image.thumbnail((1080, 1080))
+def resize_image(ig_username, ig_password, directory, image_width, image_height):
+    for filename in listdir(directory):
+        image = Image.open(os.path.join(directory, filename))
+        image.thumbnail((image_width, image_height))
         image_name, filename_extension = os.path.splitext(filename)
-        image.save(os.path.join(DIR_PATH, f'{image_name}.jpg'), format='JPEG')
+        image.save(os.path.join(directory, f'{image_name}.jpg'), format='JPEG')
 
 
-def upload_picture(ig_username, ig_password, DIR_PATH):
+def upload_picture(ig_username, ig_password, directory):
     bot = Bot()
     bot.login(username=ig_username, password=ig_password)
-    for filename in listdir(DIR_PATH):
-        bot.upload_photo(os.path.join(DIR_PATH, filename))
+    for filename in listdir(directory):
+        bot.upload_photo(os.path.join(directory, filename))
         response = bot.api.last_response
         response.raise_for_status()
 
@@ -50,7 +52,7 @@ def main():
     parser = create_parser()
     selected_function = parser.parse_args()
     try:
-        selected_function.func(ig_username, ig_password, DIR_PATH)
+        selected_function.func(ig_username, ig_password, DIR_PATH, IMAGE_WIDTH, IMAGE_HEIGHT)
     except requests.exceptions.HTTPError as error:
         exit('Ошибка:\n{0}'.format(error))
 
